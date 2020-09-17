3054f337b69f5d9284d0613ed9c0cd018764d77f
Pedro Peña <pedro@makedit.com>
2020-09-17 06:53:19 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='openwisp-makedit',
+    version='0.1',
+    packages=['ow-makedit'],
+    url='',
+    license='',
+    author='Pedro Peña',
+    author_email='pedro@makedit.com',
+    description='Makedit Openwisp2',
+    include_package_data=True,
+)
