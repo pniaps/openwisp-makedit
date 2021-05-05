58e4137040753450eee7a9f94ad644c6df9a13cd
Pedro Peña <pedro@makedit.com>
2021-05-05 18:44:02 +0200

    Add mac_suffix system variable

--- ow-makedit/apps.py
+++ ow-makedit/apps.py
@@ -5,3 +5,17 @@ import types
 class owMakeditConfig(AppConfig):
     name = 'ow-makedit'
     label = 'ow-makedit-config'
+
+    def ready(self, *args, **kwargs):
+        self.replace_device_get_context()
+
+    def replace_device_get_context(self):
+        from openwisp_controller.config.models import Config
+        _orig_config_context = Config.get_context
+        def config_get_context(self, system=False):
+            context = _orig_config_context(self, system)
+            if self._has_device():
+                context['mac_suffix'] = self.mac_address[9:11]+self.mac_address[12:14]+self.mac_address[15:17]
+            return context
+        Config.get_context = config_get_context
+
